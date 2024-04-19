ba2c9490c5c8220edb33b3335542a56e667d6441
Bui Huu Dat <dat54261001@gmail.com>
2024-04-19 17:43:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ConfigProvider } from "antd";
+import TopNavbar from "./components/TopNavbar";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Posts from "./components/Posts";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./configureStore";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ConfigProvider>
+          <main className="h-full w-screen">
+            <TopNavbar />
+            <Posts />
+          </main>
+        </ConfigProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
